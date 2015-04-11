b5f534c5215ef0f6c1413910b001446b8e034304
Shashwat Anand <shashwat.anand@gmail.com>
2015-04-11 23:31:18 +0530

    Updated plugin xml and added files

---
+++ src/com/shashwat/rcp/jobs/sample/com/shashwat/rcp/jobs/sample/navigator/FileView.java
@@ -0,0 +1,25 @@
+package com.shashwat.rcp.jobs.sample.com.shashwat.rcp.jobs.sample.navigator;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.navigator.CommonNavigator;
+
+public class FileView extends CommonNavigator {
+	public static final String ID = "com.shashwat.rcp.jobs.sample.fileview";
+
+	public FileView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/shashwat/rcp/jobs/sample/com/shashwat/rcp/jobs/sample/navigator/model/NavigatorRoot.java
@@ -0,0 +1,5 @@
+package com.shashwat.rcp.jobs.sample.com.shashwat.rcp.jobs.sample.navigator.model;
+
+public class NavigatorRoot {
+
+}
